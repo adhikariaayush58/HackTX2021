18f173031f009d61c466335451ed5e1154123e30
adhikariaayush58 <59550011+adhikariaayush58@users.noreply.github.com>
2021-10-31 09:54:05 -0500

    Frontend

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { textAlign } from '@mui/system';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form,Row,Col } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+class App extends React.Component {
+   render() {
+      return (
+         <div>    
+           <h1
+           style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems:"center"
+           }}
+           >Student Portal</h1>
+            <Form>
+               <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                  <Row>
+                     <Col sm="4"/>
+                     <Col sm="4">
+                        <Form.Control plaintext defaultValue="Search" style={{border:'1px solid black'}} />
+                     </Col>
+                     <Col sm="4">
+                     <Button variant="primary" type="submit">
+Search               </Button>
+                     </Col>
+                  </Row>
+               </Form.Group>
+             
+               
+               
+            </Form>
+            
+         </div>
+
+          
+      );
+   }
 }
 
 export default App;
+
